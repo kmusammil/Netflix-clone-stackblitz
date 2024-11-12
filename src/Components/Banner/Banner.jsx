9e944c111056a8f1db13765e6f63b7ec919cfaf1
muzammilalparamb <muzammilalparamb@gmail.com>
2024-11-12 14:20:16 +0000

    banner work on input

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner(){
             <div className="banner-container">
                 <div className="contents">
                     <h1>Unlimited movies, <br/> TV shows and more</h1>
-                    <h3>Starts at ₹149. Cancel at any time.</h3>
+                    <h2>Starts at ₹149. Cancel at any time.</h2>
                     <p>Ready to watch? Enter your email to create or restart your membership.</p>
                 </div>
                 <div className="get-started">
