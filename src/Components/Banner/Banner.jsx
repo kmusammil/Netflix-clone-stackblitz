86513088d3175f212cfcfe5114158f4b33d8991d
kmusammil <muzammilalparamb@gmail.com>
2024-11-14 10:37:53 +0530

    tailwind set up

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,27 @@
 import './Banner.css'
-function Banner(){
-    return(
+function Banner() {
+    return (
         <>
             <div className="banner-container">
                 <div className="contents">
-                    <h1>Unlimited movies, <br/> TV shows and more</h1>
+                    <h1>Unlimited movies, <br /> TV shows and more</h1>
                     <h2>Starts at ₹149. Cancel at any time.</h2>
                     <p>Ready to watch? Enter your email to create or restart your membership.</p>
                 </div>
                 <div className="get-started">
                     <input type="text" placeholder='Email Address' />
                     <button>Get Started</button>
-                    <i class="fa-solid fa-angle-right"></i>
+                    <i className="fa-solid fa-angle-right"></i>
+                </div>
+                <div className="bg-black">
+                    <div className="curved-bg">
+                        <div className="absolute inset-0 bg-cover bg-center" ></div>
+                    </div>
                 </div>
             </div>
-            
+
+
+
         </>
     )
 }
